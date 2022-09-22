dac0657fd9dddd33d97f74b9196a4e493fcf04ee
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-22 10:06:42 -0600

    created solution and completed all primary deliverables. some stretch deliverables are not yet implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const btn = document.createElement('button')
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+  
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const input = document.getElementById('new-task-description');
+    addToDo(input.value);
+    input.value = "";
+  })
+
+  const addToDo = (text) => {
+    const newItem = document.createElement('p')
+    newItem.textContent = ` ${text}  `;
+    const btn = document.createElement('button')
+    btn.textContent = " delete "
+    btn.addEventListener('click', deleteToDo);
+    newItem.appendChild(btn);
+    taskList.appendChild(newItem);
+  }
+
+  function deleteToDo(e) {
+    console.log(e);
+    e.target.parentNode.remove();
+  }
+
 });
